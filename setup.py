d31c1af8c78901ca3a664e75fd130eac06cbc7cb
Michael Elsdörfer <michael@elsdoerfer.com>
2013-10-03 20:34:34 +0200

    Add setup.py file.

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# encoding: utf8
+
+from setuptools import setup
+
+
+# Figure out the version
+import track
+version = '.'.join(map(lambda s: str(s), track.__version__))
+
+
+setup(
+    name='track',
+    version=version,
+    description="A website copier.",
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP'
+    ],
+    author='Michael Elsdörfer',
+    author_email='michael@elsdoerfer.com',
+    url='http://github.com/miracle2k/track',
+    license='BSD',
+    packages=['track'],
+    instsall_requires=[
+        'beautifulsoup4',
+        'requests'
+    ],
+    entry_points="""[console_scripts]\ntrack = track.cli:run\n"""
+)
+
